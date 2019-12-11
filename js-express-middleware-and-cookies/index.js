411d97fae6154210b9a1327a96b8d28ec69c03c7
Hano <hinsul3113@gmail.com>
2019-12-11 12:35:47 -0800

    Separate routes into a different directory

--- js-express-middleware-and-cookies/index.js
+++ js-express-middleware-and-cookies/index.js
@@ -74,67 +74,10 @@ app.use((req, res, next) => {
   next();
 });
 
-// Create a route for the root "/" path
-const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 7; // a week in milliseconds
-app.get("/", (req, res) => {
-  const cookieValue = "My new cookie";
-  res.cookie("myCookie", cookieValue, {
-    maxAge: COOKIE_MAX_AGE
-  });
+// require and use routes
+const baseRouter = require("./routes/baseRouter");
 
-  res.render("welcome");
-});
-
-// sign_in page route
-app.get("/sign_in", (req, res) => {
-  res.render("signInPage");
-});
-
-// sign_in post route
-app.post("/sign_in", (req, res) => {
-  // const username = req.body.username;
-  const { username } = req.body;
-  res.cookie("username", username, { maxAge: COOKIE_MAX_AGE });
-
-  res.redirect("/");
-});
-
-// Sign Out route
-
-app.post("/sign_out", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/");
-});
-
-// recieves at least 2 arguments:
-// 1) PATH of the resource
-// 2) A callback: the callback has two parameters
-//   a) request: Represents a HTTP Request
-//   b) response: Represents a HTTP Response
-
-// Route to handle GET "/hello_world"
-app.get("/hello_world", (request, response) => {
-  // console.log("requestObject: ", request);
-  // response.render() is used to render out a view, it accepts 3 arguments:
-  // 1) the path to the view starting from the configured directory, in this case it's "/views"
-  // 2) locals object
-  // 3) callback <-- usually you don't use this third argument
-  response.render("hello_world");
-});
-
-// Make a route to handle GET "/survey"
-app.get("/survey", (req, res) => {
-  res.render("survey");
-});
-
-// Handler for GET "/submit"
-app.get("/submit", (req, res) => {
-  // console.log(req.query);
-  res.render("submit", {
-    query: req.query,
-    subHeading: "thank you!"
-  });
-});
+app.use("/", baseRouter);
 
 // app.listen is used to start your express server. It tells express to start listening for requests at a given url
 
