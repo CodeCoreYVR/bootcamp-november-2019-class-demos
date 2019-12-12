bfbbb99b2918480a618ffa2b34c491d7a0aa732c
Hano <hinsul3113@gmail.com>
2019-12-12 09:58:43 -0800

    Generate migrations

---
+++ js-express-rest/express-rest/migrations/20191212095458_AddImageUrlToPosts.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.table("posts", table => {
+    table.string("imageUrl"); // Add column 'imageUrl' VARCHAR(255)
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table("posts", table => {
+    //   Alter table "posts"
+    table.dropColumn("imageUrl");
+  });
+};
