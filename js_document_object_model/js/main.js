f3d54838dba53bf1797e17c253373c9d1fed9116
Hano <hinsul3113@gmail.com>
2020-01-21 10:06:34 -0800

    Exercise Picking doggos

--- js_document_object_model/js/main.js
+++ js_document_object_model/js/main.js
@@ -3,3 +3,23 @@ const teamSalmonSelector = ".team.salmon";
 
 const teamSalmon = document.querySelector(teamSalmonSelector);
 const lastDoggoTeamSalmon = teamSalmon.querySelector(".doggo:last-child");
+
+const allDoggoFighters = document.querySelectorAll(".doggo.fighter");
+const divs = document.querySelectorAll("div");
+
+const bumbleBertha = document.getElementById("bumble-bertha");
+
+// Exercise: Picking Doggos
+
+// 1. Select MoneyBags Michael & Larry The Lion
+const moneyBagsAndLarry = document.querySelectorAll(
+  "#larry-the-lion, #moneybags-michael"
+);
+// 2. Select all team teal doggos but first
+const allTeamTealDoggosButFirst = document.querySelectorAll(
+  ".team.teal .doggo:not(:first-child)"
+);
+// 3. Select the second doggo in every team
+const secondDoggos = document.querySelectorAll(
+  ".team .roster .doggo:nth-child(2)"
+);
