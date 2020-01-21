c4b7963d930167fc281fa1d3980e098be744b512
Hano <hinsul3113@gmail.com>
2020-01-21 09:45:33 -0800

    Selecting DOM elements with query selectors

---
+++ js_document_object_model/js/main.js
@@ -0,0 +1,5 @@
+const toxicTim = document.querySelector("#toxic-tim");
+const teamSalmonSelector = ".team.salmon";
+
+const teamSalmon = document.querySelector(teamSalmonSelector);
+const lastDoggoTeamSalmon = teamSalmon.querySelector(".doggo:last-child");
